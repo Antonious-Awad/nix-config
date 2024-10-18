36204cb7af1f11804d4ace2b1e4647886a03d754
Antonious-Awad <antonuostony1@gmail.com>
2024-10-18 14:39:59 +0300

    init nix config

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "Personal Nix configuration with GUI applications";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }: 
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+          # Optional: you can also allow specific unfree packages instead of all
+          # allowUnfreePredicate = pkg: builtins.elem (pkg.pname or "") [
+          #   "discord"
+          #   "spotify"
+          # ];
+        };
+      };
+    in
+    {
+      packages.${system} = {
+        default = self.packages.${system}.myPackages;
+        myPackages = pkgs.buildEnv {
+          name = "my-packages";
+          paths = with pkgs; [
+            # GUI Applications
+            # firefox
+            # vscode
+            # spotify
+            discord
+            # gimp
+            # vlc
+            
+            # Terminal utilities
+            # git
+            # htop
+            # ripgrep
+            
+            # Development tools
+            # gcc
+            # python3
+          ];
+        };
+      };
+
+      # Nix configuration
+      nixConfig = {
+        experimental-features = [ "nix-command" "flakes" ];
+        substituters = [
+          "https://cache.nixos.org"
+        ];
+        trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        ];
+        auto-optimise-store = true;
+        allowed-users = [ "@wheel" ];
+      };
+    };
+}
